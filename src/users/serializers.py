b7149d4a394fbaa3656c3a5b95a930299bf39555
sansly <robertstepa017@gmail.com>
2025-02-27 20:36:46 +0300

    feat(base structure): init project structure, swagger endpoints

---
+++ src/users/serializers.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions
+from rest_framework import serializers
+
+from users.models import User
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+
+    def validate_password(self, value):
+        try:
+            validate_password(value)
+        except exceptions.ValidationError as e:
+            raise serializers.ValidationError(e.messages)
+        return value
